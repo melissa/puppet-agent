e1b7470c7b69a271b186dbe11ee9a26597b6aacd
Melissa Stone <melissa@puppetlabs.com>
2016-01-05 00:16:49 +0000

    (RE-6013) Ensure native projects build on windows

--- configs/components/cpp-pcp-client.rb
+++ configs/components/cpp-pcp-client.rb
@@ -2,9 +2,13 @@ component "cpp-pcp-client" do |pkg, settings, platform|
   pkg.load_from_json('configs/components/cpp-pcp-client.json')
   cmake = "/opt/pl-build-tools/bin/cmake"
   toolchain = "-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/pl-build-toolchain.cmake"
-  pkg.environment "PATH" => "#{settings[:bindir]}:/opt/pl-build-tools/bin:$$PATH"
+  pkg.environment "PATH" => "#{settings[:bindir]}:#{settings[:tools_root]}/bin:$$PATH"
 
   platform_flags = ''
+
+  prefix = settings[:prefix]
+  make = platform[:make]
+
   pkg.build_requires "openssl"
   if platform.is_aix?
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-gcc-5.2.0-1.aix#{platform.os_version}.ppc.rpm"
@@ -19,6 +23,25 @@ component "cpp-pcp-client" do |pkg, settings, platform|
   elsif platform.is_solaris?
     cmake = "/opt/pl-build-tools/i386-pc-solaris2.#{platform.os_version}/bin/cmake"
     toolchain = "-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/#{settings[:platform_triple]}/pl-build-toolchain.cmake"
+  elsif platform.is_windows?
+    pkg.build_requires "cmake"
+    pkg.build_requires "pl-toolchain-#{platform.architecture}"
+    pkg.build_requires "pl-boost-#{platform.architecture}"
+
+    arch = platform.architecture == "x64" ? "64" : "32"
+    make = "#{platform.drive_root}/tools/mingw#{arch}/bin/mingw32-make"
+    prefix = platform.convert_to_windows_path(settings[:prefix])
+
+    pkg.environment "MAKE" => platform.convert_to_windows_path(make)
+    pkg.environment "PATH" => "#{settings[:gcc_bindir]}:#{settings[:tools_root]}/bin:#{settings[:bindir]}:#{platform.drive_root}/Windows/system32:#{platform.drive_root}/Windows:#{platform.drive_root}/Windows/System32/Wbem:#{platform.drive_root}/Windows/System32/WindowsPowerShell/v1.0:#{platform.drive_root}/pstools"
+    pkg.environment "CYGWIN" => settings[:cygwin]
+    pkg.environment "CC" => settings[:cc]
+    pkg.environment "CXX" => settings[:cxx]
+    pkg.environment "LDFLAGS" => settings[:ldflags]
+    pkg.environment "CFLAGS" => settings[:cflags]
+
+    cmake = "#{platform.drive_root}/ProgramData/chocolatey/bin/cmake.exe -G \"MinGW Makefiles\""
+    toolchain = "-DCMAKE_TOOLCHAIN_FILE=#{platform.convert_to_windows_path(settings[:tools_root])}/pl-build-toolchain.cmake"
   else
     pkg.build_requires "pl-gcc"
     pkg.build_requires "pl-cmake"
@@ -31,19 +54,19 @@ component "cpp-pcp-client" do |pkg, settings, platform|
       #{toolchain} \
       #{platform_flags} \
           -DCMAKE_VERBOSE_MAKEFILE=ON \
-          -DCMAKE_PREFIX_PATH=#{settings[:prefix]} \
-          -DCMAKE_INSTALL_PREFIX=#{settings[:prefix]} \
-          -DCMAKE_SYSTEM_PREFIX_PATH=#{settings[:prefix]} \
+          -DCMAKE_PREFIX_PATH=#{prefix} \
+          -DCMAKE_INSTALL_PREFIX=#{prefix} \
+          -DCMAKE_SYSTEM_PREFIX_PATH=#{prefix} \
           -DBOOST_STATIC=ON \
           ."
     ]
   end
 
   pkg.build do
-    ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1)"]
+    ["#{make} -j$(shell expr $(shell #{platform[:num_cores]}) + 1)"]
   end
 
   pkg.install do
-    ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) install"]
+    ["#{make} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) install"]
   end
 end
--- configs/components/pxp-agent.rb
+++ configs/components/pxp-agent.rb
@@ -3,9 +3,15 @@ component "pxp-agent" do |pkg, settings, platform|
 
   toolchain = "-DCMAKE_TOOLCHAIN_FILE=/opt/pl-build-tools/pl-build-toolchain.cmake"
   cmake = "/opt/pl-build-tools/bin/cmake"
-  pkg.environment "PATH" => "#{settings[:bindir]}:/opt/pl-build-tools/bin:$$PATH"
+
+  pkg.environment "PATH" => "#{settings[:bindir]}:#{settings[:tools_root]}/bin:$$PATH"
 
   pkg.build_requires "openssl"
+
+  prefix = settings[:prefix]
+  install_root = settings[:install_root]
+  make = platform[:make]
+
   if platform.is_aix?
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-gcc-5.2.0-1.aix#{platform.os_version}.ppc.rpm"
     pkg.build_requires "http://pl-build-tools.delivery.puppetlabs.net/aix/#{platform.os_version}/ppc/pl-cmake-3.2.3-2.aix#{platform.os_version}.ppc.rpm"
@@ -19,6 +25,25 @@ component "pxp-agent" do |pkg, settings, platform|
 
     # PCP-87: If we build with -O3, solaris segfaults due to something in std::vector
     special_flags = "-DCMAKE_CXX_FLAGS_RELEASE='-O2 -DNDEBUG'"
+  elsif platform.is_windows?
+    pkg.build_requires "cmake"
+    pkg.build_requires "pl-toolchain-#{platform.architecture}"
+    pkg.build_requires "pl-boost-#{platform.architecture}"
+
+    arch = platform.architecture == "x64" ? "64" : "32"
+    make = "#{platform.drive_root}/tools/mingw#{arch}/bin/mingw32-make"
+    prefix = platform.convert_to_windows_path(settings[:prefix])
+
+    pkg.environment "MAKE" => platform.convert_to_windows_path(make)
+    pkg.environment "PATH" => "#{settings[:gcc_bindir]}:#{settings[:tools_root]}/bin:#{settings[:bindir]}:#{platform.drive_root}/Windows/system32:#{platform.drive_root}/Windows:#{platform.drive_root}/Windows/System32/Wbem:#{platform.drive_root}/Windows/System32/WindowsPowerShell/v1.0:#{platform.drive_root}/pstools"
+    pkg.environment "CYGWIN" => settings[:cygwin]
+    pkg.environment "CC" => settings[:cc]
+    pkg.environment "CXX" => settings[:cxx]
+    pkg.environment "LDFLAGS" => settings[:ldflags]
+    pkg.environment "CFLAGS" => settings[:cflags]
+
+    cmake = "#{platform.drive_root}/ProgramData/chocolatey/bin/cmake.exe -G \"MinGW Makefiles\""
+    toolchain = "-DCMAKE_TOOLCHAIN_FILE=#{platform.convert_to_windows_path(settings[:tools_root])}/pl-build-toolchain.cmake"
   else
     pkg.build_requires "pl-gcc"
     pkg.build_requires "pl-cmake"
@@ -30,10 +55,10 @@ component "pxp-agent" do |pkg, settings, platform|
       "#{cmake}\
       #{toolchain} \
           -DCMAKE_VERBOSE_MAKEFILE=ON \
-          -DCMAKE_PREFIX_PATH=#{settings[:prefix]} \
-          -DCMAKE_INSTALL_PREFIX=#{settings[:prefix]} \
-          -DCMAKE_SYSTEM_PREFIX_PATH=#{settings[:prefix]} \
-          -DMODULES_INSTALL_PATH=#{File.join(settings[:install_root], 'pxp-agent', 'modules')} \
+          -DCMAKE_PREFIX_PATH=#{prefix} \
+          -DCMAKE_INSTALL_PREFIX=#{prefix} \
+          -DCMAKE_SYSTEM_PREFIX_PATH=#{prefix} \
+          -DMODULES_INSTALL_PATH=#{File.join(install_root, 'pxp-agent', 'modules')} \
           #{special_flags} \
           -DBOOST_STATIC=ON \
           ."
@@ -41,16 +66,21 @@ component "pxp-agent" do |pkg, settings, platform|
   end
 
   pkg.build do
-    ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1)"]
+    ["#{make} -j$(shell expr $(shell #{platform[:num_cores]}) + 1)"]
   end
 
   pkg.install do
-    ["#{platform[:make]} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) install"]
+    ["#{make} -j$(shell expr $(shell #{platform[:num_cores]}) + 1) install"]
   end
+
+  # Due to windows, when we run the configure, we need this variable to be set
+  # in accordance with windows pathing. However, when we create the directory
+  # below, we need it in accordance with cygwin pathing.
+  install_root = settings[:prefix] if platform.is_windows?
 
   pkg.directory File.join(settings[:sysconfdir], 'pxp-agent')
   pkg.directory File.join(settings[:sysconfdir], 'pxp-agent', 'modules')
-  pkg.directory File.join(settings[:install_root], 'pxp-agent', 'spool')
+  pkg.directory File.join(install_root, 'pxp-agent', 'spool')
   pkg.directory File.join(settings[:logdir], 'pxp-agent')
 
   case platform.servicetype
@@ -72,6 +102,8 @@ component "pxp-agent" do |pkg, settings, platform|
     pkg.install_service "ext/solaris/smf/pxp-agent.xml", service_type: "network"
   when "aix"
     pkg.install_service "resources/aix/pxp-agent.service", nil, "pxp-agent"
+  when "windows"
+    puts "Service files not enabled on windows"
   else
     fail "need to know where to put #{pkg.get_name} service files"
   end
