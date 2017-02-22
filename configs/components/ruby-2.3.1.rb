edd264f6fbdfc5bb12b6d633b7431451efdfdb70
Melissa Stone <melissa@puppet.com>
2017-02-22 21:13:32 +0000

    (maint) is_osx => is_macos

--- configs/components/ruby-2.3.1.rb
+++ configs/components/ruby-2.3.1.rb
@@ -142,7 +142,7 @@ component "ruby-2.3.1" do |pkg, settings, platform|
     pkg.environment "optflags" => "-O2"
   end
 
-  if platform.is_osx?
+  if platform.is_macos?
     pkg.environment "optflags" => settings[:cflags]
   end
 
